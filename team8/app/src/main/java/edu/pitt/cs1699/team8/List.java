ff4e4ae12580a40135eb3b8561c5885bd868390b
Scott Crawford <sfc17@pitt.edu>
2018-04-02 19:52:08 -0400

    added list

--- team8/app/src/main/java/edu/pitt/cs1699/team8/List.java
+++ team8/app/src/main/java/edu/pitt/cs1699/team8/List.java
@@ -2,12 +2,60 @@ package edu.pitt.cs1699.team8;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class List extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        mAuth=FirebaseAuth.getInstance();
+
+        String user = mAuth.getCurrentUser().getUid();
+
+
+        BackendManager backendManager = new BackendManager(user);
+
+
+
+        ArrayList<String> groceriesList = backendManager.getItemsAsStringArray();
+
+
+        ListView list = findViewById(R.id.listView);
+        if(groceriesList!=null) {
+            list.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, groceriesList));
+        }
+
+
+    }
+
+    protected void singleClick(View view){
+
+    }
+
+    protected void multiClick(View view){
+
+    }
+
+    protected void clearClick(View view){
+
     }
 }
