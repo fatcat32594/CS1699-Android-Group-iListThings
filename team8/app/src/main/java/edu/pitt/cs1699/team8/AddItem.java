39ef0c281418962df99ff6a3994101831c0460cf
Jeff <jfw20@users.noreply.github.com>
2018-04-02 20:31:07 -0400

    added addClick

--- team8/app/src/main/java/edu/pitt/cs1699/team8/AddItem.java
+++ team8/app/src/main/java/edu/pitt/cs1699/team8/AddItem.java
@@ -1,13 +1,51 @@
 package edu.pitt.cs1699.team8;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class AddItem extends AppCompatActivity {
+
+    EditText itemText;
+    EditText priceText;
+    EditText quantityText;
+    BackendManager backendManager;
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+        mAuth = FirebaseAuth.getInstance();
+        backendManager = new BackendManager(mAuth.getCurrentUser().getUid());
+    }
+
+    public void addClick(){
+        itemText = findViewById(R.id.item_input);
+        priceText = findViewById(R.id.price_input);
+        quantityText = findViewById(R.id.quantity_input);
+
+        String itemName = itemText.getText().toString();
+        long itemQuantity = Long.parseLong(quantityText.getText().toString());
+        final double itemPrice = Double.parseDouble(priceText.getText().toString());
+
+        backendManager.addItem(mAuth.getCurrentUser().getUid(), itemName, itemPrice, itemQuantity);
+
+        try {
+            Intent intent = new Intent("edu.pitt.cs1699.team9.NEW_STOCK");
+            intent.putExtra("company", itemName);
+            intent.putExtra("price", itemPrice);
+            startActivity(intent);
+        }catch(Exception e){
+
+        }
     }
 }
